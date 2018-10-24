078f9811f68b50fa3b1c702477abab8660877541
nacho <ignaciodentti@gmail.com>
2018-10-24 20:13:54 -0300

    Avances EJ03

---
+++ TP4/EJ03/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ03
+{
+    class Usuario : IComparable<Usuario>
+    {
+        private string iCodigo;
+        private string iNombreCompleto;
+        private string iCorreoElectronico;
+
+
+        public string Codigo
+        {
+            get { return iCodigo; }
+            set { iCodigo = value; }
+        }
+
+        public string NombreCompleto
+        {
+            get { return iNombreCompleto; }
+            set { iNombreCompleto = value; }
+        }
+
+        public string CorreoElectronico
+        {
+            get { return iCorreoElectronico; }
+            set { iCorreoElectronico = value; }
+        }
+
+        public int CompareTo(Usuario otro)
+        {
+            return String.Compare(this.iCodigo, otro.iCodigo);
+        }
+    }
+}
